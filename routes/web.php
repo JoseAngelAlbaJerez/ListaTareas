2e380600d4c2673a2e8577d25f69f0cfe339ba68
JoseAngelAlbaJerez2 <joseangelalbaalt@gmail.com>
2023-11-10 12:54:15 -0400

    added Comments to routes

--- routes/web.php
+++ routes/web.php
@@ -5,40 +5,43 @@ use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Rutas Web
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Aquí es donde puedes registrar rutas web para tu aplicación. Estas
+| rutas son cargadas por RouteServiceProvider y todas ellas se asignarán al
+| grupo de middleware "web". ¡Haz algo grandioso!
 |
 */
-//Obtener Vista de Bienvenida 
+
+// Obtener la vista de bienvenida
 Route::get('/', function () {
     return view('/welcome');
 });
 
-//Ruta de Autentiacion
+// Ruta de autenticación
 Auth::routes();
-//Ruta Inicio
+
+// Ruta para la página de inicio
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-//Ruta Index de Tarea
-Route::get('/tasks', [App\Http\Controllers\TaskController::class,'index'])->name('index');
 
-//Ruta Crear Tarea
+// Ruta para mostrar todas las tareas
+Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('index');
+
+// Ruta para la creación de una nueva tarea
 Route::get('/tasks/create', [App\Http\Controllers\TaskController::class, 'create'])->name('task.create');
-//Ruta Guardar Tarea
-Route::post('/tasks', [App\Http\Controllers\TaskController::class,'store'])->name('task.store');
 
-//Ruta Editar Tarea
-Route::get('/tasks/{id}/edit', [App\Http\Controllers\TaskController::class,'edit'])->name('task.edit');
-//Ruta Actualizar Tarea
-Route::put('/tasks/{id}', [App\Http\Controllers\TaskController::class,'update'])->name('task.update');
-//Ruta Eliminar Tarea
-Route::delete('/tasks/{id}', [App\Http\Controllers\TaskController::class,'destroy'])->name('task.delete');
-//Ruta Mostrar Tareas
+// Ruta para guardar una nueva tarea
+Route::post('/tasks', [App\Http\Controllers\TaskController::class, 'store'])->name('task.store');
+
+// Ruta para editar una tarea existente
+Route::get('/tasks/{id}/edit', [App\Http\Controllers\TaskController::class, 'edit'])->name('task.edit');
+
+// Ruta para actualizar una tarea existente
+Route::put('/tasks/{id}', [App\Http\Controllers\TaskController::class, 'update'])->name('task.update');
+
+// Ruta para eliminar una tarea existente
+Route::delete('/tasks/{id}', [App\Http\Controllers\TaskController::class, 'destroy'])->name('task.delete');
+
+// Ruta para mostrar todas las tareas (otra vez, podría ser un duplicado)
 Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index'])->name('tasks');
-//Ruta Actualizar Tarea checkbox (En progreso)
-Route::post('/tasks/{task}/update-completed', 'TaskController@updateCompleted');
-
-
